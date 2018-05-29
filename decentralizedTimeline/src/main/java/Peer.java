f2aa5a64207afaf4182869ba5282706b55128a16
jlabm <a68646@alunos.uminho.pt>
2018-05-29 18:36:50 +0100

    Comment parts of Peer.java that need to be refactored

--- decentralizedTimeline/src/main/java/Peer.java
+++ decentralizedTimeline/src/main/java/Peer.java
@@ -3,55 +3,58 @@ import io.atomix.catalyst.concurrent.ThreadContext;
 import io.atomix.catalyst.serializer.Serializer;
 import io.atomix.catalyst.transport.Address;
 import io.atomix.catalyst.transport.Connection;
-import io.atomix.catalyst.transport.Server;
 import io.atomix.catalyst.transport.Transport;
 import io.atomix.catalyst.transport.netty.NettyTransport;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 
+// TODO: Refactor this class!
 public class Peer {
-    private String username;
+    private User user;
     private Address addr;
-    private Map<String, Address> peers;
-    private List<String> users;
     private Transport transport;
     private ThreadContext tc;
-    private Server server;
     private List<Connection> neighborConns;
+    //  private Server server;
+    //  private Map<String, Address> peers;
+    //  private List<String> users;
 
-    public Peer(String username, Address address){
-       this.username = username;
+    public Peer(Address address) {
         this.addr = address;
-       this.peers = new HashMap<String, Address>();
-       this.users = new ArrayList<String>();
-
-       this.transport = new NettyTransport();
-       this.tc = new SingleThreadContext("proto-%d", new Serializer());
-
-       this.server = this.transport.server();
+        this.transport = new NettyTransport();
+        this.tc = new SingleThreadContext("proto-%d", new Serializer());
+        this.neighborConns = new ArrayList<>();
+        // this.server = this.transport.server();
+        // this.peers = new HashMap<String, Address>();
+        // this.users = new ArrayList<String>();
     }
 
-    public void listen(Consumer<Connection> connHandler){
+    /*
+    public void listen(Consumer<Connection> connHandler) {
         tc.execute(() -> {
             this.server.listen(this.addr, connHandler);
         });
     }
+*/
 
-    public void bootstrap(Address addr) throws Exception{
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /*
+    public void bootstrap(Address addr) throws Exception {
         Connection c = tc.execute(() -> this.transport.client().connect(addr)).join().get();
-        RegisterRep regRep =
-                (RegisterRep) this.tc.execute(() ->
-                c.sendAndReceive(new RegisterReq(this.username))).join().get();
+        RegisterRep regRep = (RegisterRep) this.tc.execute(() ->
+                c.sendAndReceive(new RegisterReq())
+        ).join().get();
 
         connectToNeigbors(regRep.getLivePeers());
     }
+    */
 
-    public void connectToNeigbors(List<Address> regRep) throws Exception{
-        for(Address a: regRep){
+    public void connectToNeighbors(List<Address> regRep) throws Exception {
+        for (Address a : regRep) {
             Connection c = tc.execute(() -> transport.client().connect(a)).join().get();
             /*TODO: test if successful*/
             this.neighborConns.add(c);
@@ -61,5 +64,4 @@ public class Peer {
     /* The client functionalities like Register and Subscribe
        would be methods that would talk to the peers neighbors 
      */
-
 }
